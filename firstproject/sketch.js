a56ddbdc2b9aa161ca19531a9a02c8818d6c9d11
benjaminj341 <54912952+benjaminj341@users.noreply.github.com>
2019-09-06 14:11:34 -0600

    drawing

--- firstproject/sketch.js
+++ firstproject/sketch.js
@@ -5,13 +5,47 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let redAmount = 0;
+let redChangeAmount = 1;
+
+let greenamount = 50;
+let greenchange = 1;
+
+let blueamount = 100;
+let bluechange = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(255);
 }
 
-function draw() {
-  background(220);
-  fill(0);
+function draw() { 
+  fill(redAmount,greenamount,blueamount);
+  noStroke();
   ellipse(mouseX, mouseY, 50, 50);
+  redAmount += redChangeAmount;
+  greenamount += greenchange;
+  blueamount += bluechange;
+
+  if (redAmount === 255){
+    redChangeAmount *= -1;
+  }
+  if (redAmount === 0){
+    redChangeAmount *= -1;
+  }
+  if (greenamount === 255){
+    greenchange *= -1;
+  }
+  if (greenamount === 0){
+    greenchange *= -1;
+  }
+  if (blueamount === 255){
+    bluechange *= -1;
+  }
+  if (blueamount === 0){
+    bluechange *= -1;
+  }
+console.log(redAmount);
+console.log(blueamount);
+console.log(greenamount);
 }
