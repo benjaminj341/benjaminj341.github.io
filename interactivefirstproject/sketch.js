1a437a5ffeb9e4fe2ccaa8aa3a1c705a3d4e6bf2
benjaminj341 <54912952+benjaminj341@users.noreply.github.com>
2019-09-11 14:11:45 -0600

    interactive project start

---
+++ interactivefirstproject/sketch.js
@@ -0,0 +1,9 @@
+let gamestart = true;
+
+function setup() {
+    createCanvas(400, 400);
+    background(200);
+    if (gamestart === true){
+        image('pizza.png', x, y, width, height)
+    }
+}
