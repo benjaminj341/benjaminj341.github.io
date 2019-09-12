461ed85c02663c63f24e4a36228234e7479c11b5
benjaminj341 <54912952+benjaminj341@users.noreply.github.com>
2019-09-12 12:23:59 -0600

    change #4

--- interactivefirstproject/sketch.js
+++ interactivefirstproject/sketch.js
@@ -1,9 +1,8 @@
-let gamestart = true;
-
+let x = 200;
 function setup() {
-    createCanvas(400, 400);
-    background(200);
-    if (gamestart === true){
-        image('pizza.png', x, y, width, height)
-    }
+    createCanvas(windowWidth, windowHeight);
+    background(0);
+}
+function draw(){
+    rect(x, 750, 50, 15);
 }
