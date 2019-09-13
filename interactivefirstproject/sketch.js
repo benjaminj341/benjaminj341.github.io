f53e280d6140a3843cd73300a670b84e24ea8cc5
benjaminj341 <54912952+benjaminj341@users.noreply.github.com>
2019-09-13 14:11:31 -0600

    spaceship added

--- interactivefirstproject/sketch.js
+++ interactivefirstproject/sketch.js
@@ -1,8 +1,33 @@
 let x = 200;
-function setup() {
+let spaceman;
+let spaceinvader;
+
+
+
+function preload(){
+    spaceman = loadImage("assets/spaceshooter.png");
+}
+
+function setup(){
     createCanvas(windowWidth, windowHeight);
     background(0);
+    
 }
 function draw(){
-    rect(x, 750, 50, 15);
+    background(0);
+    image(spaceman, x, 750, 75, 75);
+    imageMode(CENTER);
+
+    let starx = random(0, windowWidth);
+    let stary = random(0, windowHeight);
+    ellipse(starx, stary, 10, 10);
+    fill(255);
+
+    if (keyIsDown(RIGHT_ARROW) && x < windowWidth){
+        x += 5;
+    }
+    if (keyIsDown(LEFT_ARROW) && x > 0){
+        x -= 5;
+    }
 }
+
