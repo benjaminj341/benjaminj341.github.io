33c15ac8bf449c848dcc27603e358f8a8aa35215
benjaminj341 <54912952+benjaminj341@users.noreply.github.com>
2019-10-30 14:12:14 -0600

    new?

--- gridproject/sketch.js
+++ gridproject/sketch.js
@@ -1,17 +1,57 @@
+let grid = [];
+let fillcolor;
+let phase = "start";
+let Xcor = 50;
+let Ycor = 20;
+
 function setup(){
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw(){
-  let grid = [];
-  for (x=0; x < width; x++){
-    grid.push([]);
-    for (y=0; y < height; y++){
-      c = random(0, 255);
+  
+}
 
-      fill(c);
-      rect(x * 10, y * 10, 10, 10);
-      grid[x].push(y);
+
+
+function mouseClicked(){
+  colorGrid();
+  phase = "play";
+}
+
+function colorGrid(){
+  for (x = 0; x < width; x++){
+    grid.push([]);
+    for (y = 0; y < height; y++){
+      if (x === Xcor && y === Ycor){
+        grid[x].push(1);
+      }
+      else {
+        grid[x].push(0);
+      }      
+    }
+  }
+  CreateGrid();
+}
+
+function CreateGrid(){
+  for (x=0; x < width; x++){
+    for (y=0; y < height; y++){
+      if (grid[x][y] === 1) {
+        fill(255)
+      }
+      else {
+        fill(0);
+      }    
+      rect(x * 10, y * 10, 10, 10);    
+    }
+  }
+}
+
+function keyPressed() {
+  if (phase === "play"){
+    if (keyCode === 68){
+      Xcor += 1;
     }
   }
 }
