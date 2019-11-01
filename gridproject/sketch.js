7b42928b3cc8193d89487327f5363a740bbd057f
benjaminj341 <54912952+benjaminj341@users.noreply.github.com>
2019-11-01 14:13:50 -0600

    cahnges??

--- gridproject/sketch.js
+++ gridproject/sketch.js
@@ -1,25 +1,33 @@
 let grid = [];
 let fillcolor;
-let Xcor = 50;
-let Ycor = 20;
+let Xcor = 10;
+let Ycor = 10;
+let rows = 400;
+let cols = 400;
+let moveMode = "right";
+
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
+  colorGrid();
+  
 }
 
 function draw(){
-  colorGrid();
+  CreateGrid();
+  let t1 = millis();
 
-  if (millis() === 10000){
-
+  if (millis() - t1 >= 1000){
+    update();
+    t1 = millis();
   }
 }
 
 
 function colorGrid(){
-  for (x = 0; x < width; x++){
+  for (x = 0; x < cols; x++){
     grid.push([]);
-    for (y = 0; y < height; y++){
+    for (y = 0; y < rows; y++){
       if (x === Xcor && y === Ycor){
         grid[x].push(1);
       }
@@ -28,12 +36,12 @@ function colorGrid(){
       }      
     }
   }
-  CreateGrid();
+  
 }
 
 function CreateGrid(){
-  for (x=0; x < width; x++){
-    for (y=0; y < height; y++){
+  for (x=0; x < cols; x++){
+    for (y=0; y < rows; y++){
       if (grid[x][y] === 1) {
         fill(255);
       }
@@ -47,21 +55,42 @@ function CreateGrid(){
 
 function keyTyped() {
   if (key === 'd'){
-    grid[Xcor][Ycor] = grid[Xcor + 1][Ycor];
+    moveMode = "right";
   }
   
   else if (key === 'w'){
-    grid = [];
-    Ycor -= 1;   
+    moveMode = "up";
   }
 
   else if (key === 'a'){
-    grid = [];
-    Xcor -= 1;   
+    moveMode = "left";
   }
 
   else if (key === 's'){
-    grid = [];
-    Ycor += 1;   
+    moveMode = "down";
   }
+}
+
+function update(){
+  if (moveMode === "down"){
+    grid[Xcor][Ycor] = 0;
+    Ycor += 1;
+    grid[Xcor][Ycor] = 1;
+  }
+  else if (moveMode === "up"){
+    grid[Xcor][Ycor] = 0;
+    Ycor -= 1;
+    grid[Xcor][Ycor] = 1;
+  }
+  else if (moveMode === "left"){
+    grid[Xcor][Ycor] = 0;
+    Xcor -= 1;
+    grid[Xcor][Ycor] = 1;
+  }
+  else if (moveMode === "right"){
+    grid[Xcor][Ycor] = 0
+    Xcor += 1;
+    grid[Xcor][Ycor] = 1;
+    console.log(1);
+  } 
 }
